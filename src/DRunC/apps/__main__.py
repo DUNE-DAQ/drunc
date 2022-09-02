a155bf0cbe79e37d7c4ec270ae7a815284ee7aff
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-09-02 10:23:39 +0100

    main -> __main__

--- src/DRunC/apps/__main__.py
+++ src/DRunC/apps/__main__.py
@@ -5,27 +5,28 @@ Command Line Interface for NanoRC
 """
 
 def main():
-    print('hello world')
-    # from rich.logging import RichHandler
+    from rich.logging import RichHandler
+    import logging
+    from rich.console import Console
 
-    # logging.basicConfig(
-    #     level="INFO",
-    #     format="%(message)s",
-    #     datefmt="[%X]",
-    #     handlers=[RichHandler(rich_tracebacks=True)]
-    # )
+    logging.basicConfig(
+        level="INFO",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
     
-    # console = Console()
-    # obj = RCContext(console)
+    console = Console()
+    obj = RCContext(console)
 
-    # try:
-    #     cli(obj=obj, show_default=True)
-    # except Exception as e:
-    #     console.log("[bold red]Exception caught[/bold red]")
-    #     if not obj.print_traceback:
-    #         console.log(e)
-    #     else:
-    #         console.print_exception()
+    try:
+        cli(obj=obj, show_default=True)
+    except Exception as e:
+        console.log("[bold red]Exception caught[/bold red]")
+        if not obj.print_traceback:
+            console.log(e)
+        else:
+            console.print_exception()
 
 if __name__ == '__main__':
     main()
