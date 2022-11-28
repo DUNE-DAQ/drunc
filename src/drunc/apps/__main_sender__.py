14d215ee157ea95ec379d445f8b7ccb518b0c684
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-11-28 12:57:11 +0000

    rename child

--- src/drunc/apps/__main_sender__.py
+++ src/drunc/apps/__main_sender__.py
@@ -7,8 +7,7 @@ def main() -> None:
     from drunc.interface.rc_shell import rc_shell, RCContext
     from drunc.controller.controller import Controller
     ctrlr = Controller('top')
-    # for p in range(30):
-    ctrlr.add_controlled_children('child', 'localhost:123')
+    ctrlr.add_controlled_children('child1', 'localhost:123')
     ctrlr.add_controlled_children('child2', 'localhost:124')
     context = RCContext(ctrlr)
     rc_shell(obj = context)
