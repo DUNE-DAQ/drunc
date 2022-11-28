8f53cf593d4e1cc200b1ea088ec1af72d1e12b90
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-11-28 17:47:16 +0000

    reduce work but improve fault intolerence

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+for FILE in proto/*.proto;
+do
+    echo "Generating protocols for ${FILE}"
+    python -m grpc_tools.protoc -I./proto --python_out=src/drunc/communication --grpc_python_out=src/drunc/communication ${FILE}
+    FILE_NAME=$(basename -- "$FILE")
+    CLASS_NAME="${FILE_NAME%.*}"
+    PY_OUT_FILE_NAME="src/drunc/communication/${CLASS_NAME}_pb2_grpc.py"
+    # echo "FILE_NAME        " $FILE_NAME
+    # echo "CLASS_NAME       " $CLASS_NAME
+    # echo "PY_OUT_FILE_NAME " $PY_OUT_FILE_NAME
+    # echo "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/"
+    sed -ie "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/" ${PY_OUT_FILE_NAME}
+done
 
-python -m grpc_tools.protoc -I./proto --python_out=src/drunc/communication --grpc_python_out=src/drunc/communication ./proto/command.proto
+
