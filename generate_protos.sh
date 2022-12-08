4f3ee733a71ee980746982c50324508960c647e9
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-12-08 16:18:02 +0000

    actually, I don't want the printout

--- generate_protos.sh
+++ generate_protos.sh
@@ -6,10 +6,10 @@ do
     FILE_NAME=$(basename -- "$FILE")
     CLASS_NAME="${FILE_NAME%.*}"
     PY_OUT_FILE_NAME="src/drunc/communication/${CLASS_NAME}_pb2_grpc.py"
-    echo "FILE_NAME        " $FILE_NAME
-    echo "CLASS_NAME       " $CLASS_NAME
-    echo "PY_OUT_FILE_NAME " $PY_OUT_FILE_NAME
-    echo "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/"
+    # echo "FILE_NAME        " $FILE_NAME
+    # echo "CLASS_NAME       " $CLASS_NAME
+    # echo "PY_OUT_FILE_NAME " $PY_OUT_FILE_NAME
+    # echo "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/"
     sed -i '' "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/" ${PY_OUT_FILE_NAME}
 done
 
