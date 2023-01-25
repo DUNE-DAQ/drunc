a7c5149d463b7b2e761155cf7c6b2f954d8181f9
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-01-25 18:27:57 +0100

    chuck the usless

--- src/drunc/commandcomm/command_sender.py
+++
@@ -1,9 +0,0 @@
-
-
-class CommandSender:
-    def __init__(self):
-        from drunc.commandcomm.grpc.sender import GRPCCommandSender
-        self.sender = GRPCCommandSender()
-
-    def send(self, message:str='') -> None:
-        self.sender('yo pierre', 'localhost:3100')
