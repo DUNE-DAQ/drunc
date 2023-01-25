dde87e64e8be1c61bd8a598511529159cb6e7338
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-01-25 17:35:09 +0100

    rename command -> controller

--- src/drunc/communication/child_channel.py
+++ src/drunc/communication/child_channel.py
@@ -1,19 +1,19 @@
 import asyncio
 import grpc
 from typing import Optional
-from drunc.communication.command_pb2 import Command, CommandResponse, Ping
-from drunc.communication.command_pb2_grpc import CommandProcessorStub
+from drunc.communication.controller_pb2 import Command, CommandResponse
+from drunc.communication.controller_pb2_grpc import ControllerStub
 
 class ChildChannel():
     # def __init__(self, cmd_address:str, status_address:str):
-    def __init__(self, cmd_address:str):
+    def __init__(self, config:dict):
         # self.sender_uri = sender_uri
         # self.user = user
-        self.cmd_address = cmd_address # TODO Multiplex
+        self.cmd_address = config['cmd_address'] # TODO Multiplex
         self.status_address = status_address
         self.command_channel = grpc.aio.insecure_channel(self.cmd_address)
         # self.status_channel = grpc.aio.insecure_channel(self.status_address)
-        self.command_stub = CommandProcessorStub(self.command_channel)
+        self.controller_stub = ControllerStub(self.command_channel)
         self.status_stub = RetrieveStatusStub(self.command_channel)
 
     def close(self):
@@ -23,9 +23,9 @@ class ChildChannel():
     async def send_command(self, command:Command) -> CommandResponse:
         print(f'Sending_command {command.command_name}, state of the channel {self.command_channel.get_state()}')
 
-        async for response in self.command_stub.execute_command(command):
+        async for response in self.controller_stub.execute_command(command):
             yield response
 
-    async def ping(self, ping:Ping) -> Ping:
-        print(f'pinging state of the channel {self.command_channel.get_state()}')
-        yield self.ping(ping)
+    # async def ping(self, ping:Ping) -> Ping:
+    #     print(f'Pinging state of the channel {self.command_channel.get_state()}')
+    #     yield self.ping(ping)
