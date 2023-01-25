9df8a8de9c678623a9b1a090c16ece3d041fa4b0
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-01-25 17:35:52 +0100

    not sure if this does anything...

--- generate_protos.sh
+++ generate_protos.sh
@@ -2,7 +2,7 @@
 for FILE in proto/*.proto;
 do
     echo "Generating protocols for ${FILE}"
-    python -m grpc_tools.protoc -I./proto --python_out=src/drunc/communication --grpc_python_out=src/drunc/communication ${FILE}
+    python -m grpc_tools.protoc --experimental_allow_proto3_optional -I./proto --python_out=src/drunc/communication --grpc_python_out=src/drunc/communication ${FILE}
     FILE_NAME=$(basename -- "$FILE")
     CLASS_NAME="${FILE_NAME%.*}"
     PY_OUT_FILE_NAME="src/drunc/communication/${CLASS_NAME}_pb2_grpc.py"
