5b3881d20c5719c95c89650cb64cd3a154c7da22
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-03-03 07:54:24 +0000

    remove pylogger imports, add help and a switch to only follow along

--- src/drunc/authoriser/dummy_authoriser.py
+++ src/drunc/authoriser/dummy_authoriser.py
@@ -1,8 +1,5 @@
-from drunc.core.pylogger import PyLogger
 from drunc.communication.controller_pb2 import Token
 from drunc.utils.utils import setup_fancy_logging
-
-
 
 # TODO: Should be communicating over network
 
@@ -12,11 +9,11 @@ class DummyAuthoriser:
         self.log = setup_fancy_logging("Controller")
         self.log.info(f'DummyAuthoriser ready')
 
-    
+
     def is_authorised(self, token:Token, action:str) -> bool:
         self.log.info(f'Authorising {token.text} to {action}')
         return True
-    
+
 
     def authorised_actions(self, token:Token) -> list[str]:
         self.log.info(f'Grabbing authorisations for {token.text}')
@@ -26,6 +23,6 @@ class DummyAuthoriser:
 def main():
     a = DummyAuthoriser()
     print(a.is_authorised())
-    
+
 if __name__ == '__main__':
     main()
--- src/drunc/interface/controller_shell.py
+++ src/drunc/interface/controller_shell.py
@@ -1,10 +1,9 @@
 import click
 import click_shell
 from drunc.controller.controller import Controller
-from drunc.controller.broadcast_receiver import BroadcastReceiver
+from drunc.interface.stdout_broadcast_handler import StdoutBroadcastHandler
 from drunc.communication.controller_pb2 import Command, Token, BroadcastRequest, GenericResponse, ResponseCode
 import grpc
-from drunc.core.pylogger import PyLogger
 from drunc.utils.utils import now_str, setup_fancy_logging
 
 class ControllerContext:
@@ -13,7 +12,7 @@ class ControllerContext:
         self.print_traceback = True
         self.controller = None
         if status_receiver_port is None: return
-        self.status_receiver = BroadcastReceiver(
+        self.status_receiver = StdoutBroadcastHandler(
             port = status_receiver_port
         )
         from threading import Thread
@@ -22,11 +21,12 @@ class ControllerContext:
 
 
 @click_shell.shell(prompt='drunc-controller > ', chain=True)
-@click.argument('controller-address', type=str)
-@click.argument('this-port', type=int)
+@click.argument('controller-address', type=str)#, help='Which address the controller is running on')
+@click.argument('this-port', type=int)#, help='Which port to use for receiving status')
+@click.option('--just-watch', type=bool, default=False, is_flag=True, help='If one just doesn\'t want to take control of the controller')
 @click.pass_obj
 @click.pass_context
-def controller_shell(ctx, obj:ControllerContext, controller_address:str, this_port:int) -> None:
+def controller_shell(ctx, obj:ControllerContext, controller_address:str, this_port:int, just_watch:bool) -> None:
     obj = ControllerContext(this_port)
     import time
     while obj.status_receiver.ready is False:
@@ -54,10 +54,13 @@ def controller_shell(ctx, obj:ControllerContext, controller_address:str, this_po
         obj.log.error('Could not add this shell to the broadcast list.')
         obj.log.error(response.response_text)
         obj.log.error('Exiting.')
+        obj.status_receiver.stop()
+        obj.server_thread.join()
         exit(1)
     obj.log.info(response.response_text)
 
     def cleanup():
+        # remove the shell from the controller broadcast list
         if added_to_broadcast:
             obj.log.debug('Removing this shell from the broadcast list.')
             response = obj.controller.remove_from_broadcast_list(
@@ -76,7 +79,9 @@ def controller_shell(ctx, obj:ControllerContext, controller_address:str, this_po
 
     ctx.call_on_close(cleanup)
 
-
+    # If we are just interested in watching the controller, then we are done here
+    if just_watch:
+        return
 
     # then take control of the controller
     # generic_response = obj.controller.take_control(
