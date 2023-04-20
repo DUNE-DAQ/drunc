762f5defcd9be8dbbbbfca285eddd1e84aee11ab
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-04-20 07:41:18 +0100

    better way to generate useless data

--- data/generate-boot-conf.py
+++ data/generate-boot-conf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+from rich.pretty import pprint as print
 
 def get_controller_instance(name, port):
     return {
@@ -24,7 +25,7 @@ instances = [get_controller_instance(f'topcontroller', 3600)]
 
 import random
 random.seed(10)
-nsystem = 4
+nsystem = 1
 
 for i in range(nsystem):
     instances.append(get_controller_instance(f'controller{i}', 3601+i))
@@ -127,30 +128,64 @@ level1 = []
 level2 = {}
 
 for instance in boot_data['instances']:
+    print(instance)
     if   'topcontroller' in instance['name']: continue
-    elif 'controller'    in instance['name']: level1.append(instance['name'])
-    elif 'app'           in instance['name']:
+    elif 'controller'    in instance['name']:
+        level1.append({
+            'name': instance['name'],
+            'cmd_address': f'localhost:{instance["port"]}'
+        })
+    elif 'app'in instance['name']:
         controller_name = f'controller{instance["name"][3]}'
         if controller_name in level2:
-            level2[controller_name].append(instance['name'])
+            level2[controller_name].append({
+                'name': instance['name'],
+                'cmd_address': f'localhost:{instance["port"]}'
+            })
         else:
-            level2[controller_name] = [instance['name']]
+            level2[controller_name] = [{
+                'name': instance['name'],
+                'cmd_address': f'localhost:{instance["port"]}'
+            }]
 
 print(level1)
+
+print('')
+print('')
+print('')
+level2 = {}
+
 print(level2)
-def generate_conf_data(controller_name:str, children:list[str]) -> None:
+def generate_conf_data(controller_name:str, children_controller:list, children_app:list) -> None:
     with open(f'{controller_name}-conf.json', 'w') as f:
         json.dump (
-            {'children': children},
+            {
+                'children_controllers': children_controller,
+                'apps': children_app,
+            },
             f,
             indent=4,
         )
 
 
-generate_conf_data('topcontroller', level1)
+generate_conf_data(
+    controller_name = 'topcontroller',
+    children_controller = level1,
+    children_app = []
+)
 
 for subcontroller in level1:
-    generate_conf_data(subcontroller, level2[subcontroller])
+    generate_conf_data(
+        controller_name = subcontroller['name'],
+        children_controller = [],
+        children_app = level2.get(subcontroller['name'], [])
+    )
 
-    for app in level2[subcontroller]:
-        generate_conf_data(app, {})
+    for app in level2.get(subcontroller['name'], []):
+        print(app)
+
+        generate_conf_data(
+            controller_name = app,
+            children_controller = [],
+            children_app = []
+        )
