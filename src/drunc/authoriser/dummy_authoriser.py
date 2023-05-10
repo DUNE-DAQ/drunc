07b7c680827ca124499dbfa7c99d9fb3a648a26f
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-05-10 08:25:20 -0400

    better authoriser

--- src/drunc/authoriser/dummy_authoriser.py
+++ src/drunc/authoriser/dummy_authoriser.py
@@ -1,17 +1,23 @@
-from drunc.communication.controller_pb2 import Token
-from drunc.utils.utils import setup_fancy_logging
+from drunc.utils.utils import get_logger
+
+from druncschema.token_pb2 import Token
+from druncschema.authoriser_pb2 import ActionType, SystemType, AuthoriserRequest
 
 # TODO: Should be communicating over network
 
 # The Rolls Royce of the authoriser systems
 class DummyAuthoriser:
-    def __init__(self):
-        self.log = setup_fancy_logging("Controller")
+    def __init__(self, configuration:dict, system:SystemType):
+        self.log = get_logger("Controller")
         self.log.info(f'DummyAuthoriser ready')
+        self.configuration = configuration
+        self.command_actions = {} # Dict[str, ActionType]
+        self.system = system
 
 
-    def is_authorised(self, token:Token, action:str) -> bool:
-        self.log.info(f'Authorising {token.token} to {action}')
+    def is_authorised(self, token:Token, command:str) -> bool:
+        action_type = self.command_actions.get(command, ActionType.UNSPECIFIED)
+        self.log.info(f'Authorising {token.token} to {command} ({action_type})')
         return True
 
 
