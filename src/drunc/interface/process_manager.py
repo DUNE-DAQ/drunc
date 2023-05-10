d187cb14b1a0b33c7f0898f7f679d8b2427cfd8c
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-05-10 09:25:37 -0400

    graceful shutdown and druncschema

--- src/drunc/interface/process_manager.py
+++ src/drunc/interface/process_manager.py
@@ -13,6 +13,8 @@ def process_manager_cli(pm_conf:str):
     console = Console()
     console.print(f'Using \'{pm_conf}\' as the ProcessManager configuration')
     pm_conf_data = None
+    from drunc.utils.utils import setup_fancy_logging
+    setup_fancy_logging()
 
     with open(pm_conf) as f:
         import json
@@ -25,18 +27,23 @@ def process_manager_cli(pm_conf:str):
     async def serve(address:str) -> None:
         if not address:
             raise RuntimeError('The address on which to expect commands/send status wasn\'t specified')
-        from drunc.communication.process_manager_pb2_grpc import add_ProcessManagerServicer_to_server
+        from druncschema.process_manager_pb2_grpc import add_ProcessManagerServicer_to_server
+
         server = grpc.aio.server()
         add_ProcessManagerServicer_to_server(pm, server)
         server.add_insecure_port(address)
+
         await server.start()
+
         console.print(f'ProcessManager was started on {address}')
+
         async def server_shutdown():
             console.print("Starting shutdown...")
             # Shuts down the server with 5 seconds of grace period. During the
             # grace period, the server won't accept new connections and allow
             # existing RPCs to continue within the grace period.
             await server.stop(5)
+            pm.terminate()
 
         _cleanup_coroutines.append(server_shutdown())
         await server.wait_for_termination()
@@ -44,7 +51,9 @@ def process_manager_cli(pm_conf:str):
     loop = asyncio.get_event_loop()
 
     try:
-        loop.run_until_complete(serve(pm_conf_data['address']))
+        loop.run_until_complete(
+            serve(pm_conf_data['address'])
+        )
     except Exception as e:
         console.print_exception()
     finally:
