b741d53c845c5de390b64e6c41836c9fc8f21f78
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-05-11 11:37:34 -0400

    add the requirements here too

--- setup.py
+++ setup.py
@@ -6,7 +6,19 @@ setup(
     package_data={
         'drunc': []
     },
-    install_requires=[],
+    install_requires=[
+        "aiostream",
+        "click",
+        "click_shell",
+        "grpcio",
+        "grpcio_tools",
+        "googleapis-common-protos",
+        "grpcio-status",
+        "nest_asyncio",
+        "rich",
+        "sh",
+        "druncschema"
+    ],
     extras_require={"develop": [
         "ipdb",
         "ipython"
