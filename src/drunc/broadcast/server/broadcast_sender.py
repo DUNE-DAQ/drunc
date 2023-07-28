01e8ebc1c51077c8a2c6886f171c4bd33633b486
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-07-28 17:53:32 +0200

    if nothing is specified, don't broadcast

--- src/drunc/broadcast/server/broadcast_sender.py
+++ src/drunc/broadcast/server/broadcast_sender.py
@@ -6,7 +6,11 @@ class BroadcastSenderTechnologyUnknown(Exception):
 
 class BroadcastSender:
     def __init__(self, configuration:dict={}):
-        self.impl_technology = configuration['type']
+        self.impl_technology = configuration.get('type')
+
+        if self.impl_technology is None:
+            return
+
         match self.impl_technology:
             case 'kafka':
                 from drunc.broadcast.server.kafka_sender import KafkaSender
@@ -15,9 +19,13 @@ class BroadcastSender:
                 from drunc.broadcast.server.grpc_servicer import GRCPBroadcastSender
                 self.implementation = GRCPBroadcastSender(configuration)
             case _:
-                raise
+                raise BroadcastSenderTechnologyUnknown(self.impl_technology)
 
     def broadcast(self, message, btype):
+        if self.impl_technology is None:
+            # nice and easy case
+            return
+
         from druncschema.broadcast_pb2 import BroadcastMessage
         from druncschema.generic_pb2 import PlainText
         from drunc.utils.grpc_utils import pack_to_any
@@ -29,4 +37,5 @@ class BroadcastSender:
             data = any,
         )
         bm.type = btype
+
         self.implementation._send(bm)
