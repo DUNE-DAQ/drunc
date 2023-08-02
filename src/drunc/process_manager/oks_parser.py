3e52d89186e685462af09605247eebde4baa6bdf
Gordon Crone <g.crone@ucl.ac.uk>
2023-08-02 13:56:12 +0100

    Add segment controllers to list of apps

--- src/drunc/process_manager/oks_parser.py
+++ src/drunc/process_manager/oks_parser.py
@@ -13,16 +13,26 @@ def process_variables(variables, envDict):
     else:
       if item.className() == 'Variable':
         envDict[item.name] = item.value
- 
+
 # Recursively process all Segments in given Segment extracting Applications
 def process_segment(db, session, segment):
-  controller = segment.controller.id
-
   # Get default environment from Session
   defenv = {}
   process_variables(session.environment, defenv)
 
   apps = []
+
+  # Add controller for this segment to list of apps
+  controller = segment.controller
+  appenv = defenv
+  process_variables(controller.applicationEnvironment, appenv)
+  host = controller.runs_on.runs_on.id
+  apps.append({"name": controller.id,
+               "type": controller.application_name,
+               "args": controller.commandline_parameters,
+               "restriction": host,
+               "host": host,
+               "env": appenv})
 
   # Recurse over nested segments
   for seg in segment.segments:
@@ -32,12 +42,9 @@ def process_segment(db, session, segment):
   for app in segment.applications:
     #print()
     if not coredal.component_disabled(db._obj, session.id, app.id):
-      #print(f"Controller: {controller}, App: {app}")
-
       appenv = defenv
       # Override with any app specific environment from Application
       process_variables(app.applicationEnvironment, appenv)
-      #print(f"Application environment={appenv}")
 
       host = app.runs_on.runs_on.id
       apps.append({"name": app.id,
@@ -56,3 +63,19 @@ def process_services(session):
     if isinstance(srv, dal.Application) and srv.enabled:
       services.append((srv.className(), srv.runs_on))
   return services
+
+
+# Search segment and all contained segments for apps controlled by
+# given controller
+def find_controlled_apps(mycontroller, segment):
+  apps = []
+  if segment.controller.id == mycontroller:
+    for app in segment.applications:
+      apps.append(app.id)
+    for seg in segment.segments:
+      apps.append(seg.controller.id)
+  else:
+    for seg in segment.segments:
+      aps.append(find_controlled_apps(mycontroller, seg))
+  return apps
+
