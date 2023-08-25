eb63935807d4de449a0dd717895bbf0720a3006d
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-08-25 20:01:07 +0200

    carbon copy some of the process manager shell

--- src/drunc/controller/interface/controller_shell.py
+++ src/drunc/controller/interface/controller_shell.py
@@ -1,8 +1,6 @@
 import click
 import click_shell
 from drunc.controller.controller import Controller
-from drunc.interface.stdout_broadcast_handler import StdoutBroadcastHandler
-from druncschema.controller_pb2 import BroadcastMessage, Level, PlainText, BroadcastRequest, StringStringMap, Location, LocationList
 from druncschema.request_response_pb2 import Request, Response
 from druncschema.token_pb2 import Token
 import drunc.controller.exceptions as ctler_excpt
@@ -14,11 +12,14 @@ from grpc_status import rpc_status
 
 
 class ControllerContext:
-    def __init__(self, status_receiver_port:int=None) -> None:
+    def __init__(self,  ctler_conf:str=None, print_traceback:bool=False) -> None:
         from logging import getLogger
         self.log = getLogger("ControllerShell")
         self.print_traceback = True
         self.controller = None
+
+        if ctler_conf is None:
+            return
 
         import os
         user = os.getlogin()
@@ -28,31 +29,37 @@ class ControllerContext:
             user_name = user
         )
 
-        self.broadcasted_to = False
+        ctler_conf_data = {}
+        with open(ctler_conf) as f:
+            import json
+            ctler_conf_data = json.loads(f.read())
 
-        if status_receiver_port is None: return
-
-        self.status_receiver = StdoutBroadcastHandler(
-            port = status_receiver_port
+        from drunc.broadcast.client.kafka_stdout_broadcast_handler import KafkaStdoutBroadcastHandler
+        from druncschema.broadcast_pb2 import BroadcastMessage
+        self.status_receiver = KafkaStdoutBroadcastHandler(
+            conf = ctler_conf_data['broadcaster'],
+            topic = 'ProcessManager',
+            message_format = BroadcastMessage,
         )
 
-        from threading import Thread
-        self.server_thread = Thread(target=self.status_receiver.serve, name=f'serve_thread')
-        self.server_thread.start()
+    def terminate(self):
+        self.status_receiver.stop()
 
-        import time
-        while self.status_receiver.ready is False:
-            time.sleep(0.1)
+    def print(self, text):
+        self._console.print(text)
+
+    def rule(self, text):
+        self._console.rule(text)
 
 
 @click_shell.shell(prompt='drunc-controller > ', chain=True)
-@click.argument('controller-address', type=str)#, help='Which address the controller is running on')
-@click.argument('this-port', type=int)#, help='Which port to use for receiving status')
-@click.option('--just-watch', type=bool, default=False, is_flag=True, help='If one just doesn\'t want to take control of the controller')
+# @click.argument('controller-address', type=str)#, help='Which address the controller is running on')
+# @click.argument('this-port', type=int)#, help='Which port to use for receiving status')
+# @click.option('--just-watch', type=bool, default=False, is_flag=True, help='If one just doesn\'t want to take control of the controller')
+@click.argument('conf', type=click.Path(exists=True))
 @click.pass_context
-def controller_shell(ctx, controller_address:str, this_port:int, just_watch:bool) -> None:
-    ctx.obj = ControllerContext(this_port)
-
+def controller_shell(ctx, conf) -> None:#controller_address:str, this_port:int, just_watch:bool) -> None:
+    ctx.obj = ControllerContext(conf)
 
     # first add the shell to the controller broadcast list
     from druncschema.controller_pb2_grpc import ControllerStub
