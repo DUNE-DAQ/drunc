55f0498f7732265ee808799a405962155f8ca2bc
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-09-20 12:54:04 +0200

    correct the status message creation

--- src/drunc/controller/utils.py
+++ src/drunc/controller/utils.py
@@ -68,13 +68,15 @@ from drunc.controller.stateful_node import StatefulNode
 
 def get_status_message(stateful:StatefulNode):
     from druncschema.controller_pb2 import Status
+    state_string = stateful.get_node_operational_state()
+    if state_string != stateful.get_node_operational_sub_state():
+        state_string += f' ({stateful.get_node_operational_sub_state()})'
 
     return Status(
         name = stateful.name,
-        #uri = stateful.uri,
-        ping = True,
-        state = stateful.fsm.current_state,
-        in_error = stateful.in_error,
-        included = stateful.included,
+        state = state_string,
+        sub_state = stateful.get_node_operational_sub_state(),
+        in_error = stateful.node_is_in_error(),
+        included = stateful.node_is_included(),
     )
 
