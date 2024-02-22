e38b68a43d41b75a62d0e15161039c6c5afdb887
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-02-22 13:05:12 +0100

    correctly parse the controller conf

--- src/drunc/controller/configuration.py
+++ src/drunc/controller/configuration.py
@@ -1,4 +1,12 @@
 from drunc.utils.configuration_utils import ConfigurationHandler
 
 class ControllerConfiguration(ConfigurationHandler):
-    pass
+    def __init__(self, uid, configuration):
+        self.uid = uid
+        super().__init__(configuration)
+
+    def _parse_oks(self, oks_conf):
+        import oksdbinterfaces
+        dal = oksdbinterfaces.dal.module('x', 'schema/coredal/dunedaq.schema.xml')
+        db = oksdbinterfaces.Configuration("oksconfig:" + oks_conf)
+        return db.get_dal(class_name="RCApplication", uid=self.uid)
