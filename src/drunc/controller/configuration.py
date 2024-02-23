75c16dc6728fd20d4719e4cf6d06b5dba4c7654e
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-02-23 11:40:27 +0100

    add children factory method

--- src/drunc/controller/configuration.py
+++ src/drunc/controller/configuration.py
@@ -1,4 +1,6 @@
-from drunc.utils.configuration_utils import ConfigurationHandler
+from drunc.utils.configuration_utils import ConfigurationHandler, ConfTypes, ConfData
+from drunc.exceptions import DruncSetupException
+from drunc.controller.children_interface.child_node import ChildNode, ChildNodeType
 
 class ControllerConfiguration(ConfigurationHandler):
     def __init__(self, uid, configuration):
@@ -10,3 +12,68 @@ class ControllerConfiguration(ConfigurationHandler):
         dal = oksdbinterfaces.dal.module('x', 'schema/coredal/dunedaq.schema.xml')
         db = oksdbinterfaces.Configuration("oksconfig:" + oks_conf)
         return db.get_dal(class_name="RCApplication", uid=self.uid)
+
+
+
+    def get_children(self):
+        if self.conf.type not in [ConfTypes.PyObject, ConfTypes.OKSObject]:
+            raise DruncSetupException('Controller Configuration was not parsed correctly')
+
+
+        match self.conf.type:
+            case ConfTypes.OKSObject:
+                return self.__get_children_from_oksobj()
+            case ConfTypes.PyObject:
+                return self.__get_children_from_pyobj()
+
+
+    def __get_children_from_pyobj(self):
+        children = []
+
+        for child in self.data.children:
+            if child.type == 'rest-api': # already some hacking
+                new_node = ChildNode.get_child(
+                    name = child.name,
+                    configuration = child,
+                    fsm_configuration = self.configuration.fsm
+                )
+
+            else:
+                new_node = ChildNode.get_child(
+                    name = child['name'],
+                    configuration = child,
+                )
+            children.append(new_node)
+
+        return children
+
+    @staticmethod
+    def __get_children_type_from_cli(CLAs:list[str]) -> ChildNodeType:
+        for CLA in CLAs:
+            if "rest://" in CLA:
+                return ChildNodeType.REST_API
+        return ChildNodeType.gRPC
+
+
+    def __get_children_from_oksobj(self):
+        children = []
+        for segment in self.data.segments:
+            new_node = ChildNode.get_child(
+                type = ControllerConfiguration.__get_children_type_from_cli(segment.controller.commandline_parameters),
+                name = segment.id,
+                configuration = ConfData(segment.controller, self.conf.type),
+                fsm_configuration = ConfData(self.configuration.fsm, self.conf.type), # TODO, this should be segment.fsm, at some point
+            )
+            children.append(new_node)
+
+        for app in self.data.applications:
+            new_node = ChildNode.get_child(
+                type = ControllerConfiguration.__get_children_type_from_cli(app.commandline_parameters),
+                name = app.id,
+                configuration = ConfData(app, self.conf.type),
+            )
+            children.append(new_node)
+
+        return children
+
+
