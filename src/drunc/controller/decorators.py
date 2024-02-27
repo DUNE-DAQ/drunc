26b8f5faf1646f9a0cce2b96bd3234c03d9ed19f
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-02-27 17:38:35 +0100

    exception update

--- src/drunc/controller/decorators.py
+++ src/drunc/controller/decorators.py
@@ -7,7 +7,7 @@ def in_control(cmd):
     def wrap(obj, request):
         if not obj.actor.token_is_current_actor(request.token):
             from drunc.controller.exceptions import OtherUserAlreadyInControl
-            raise OtherUserAlreadyInControl()
+            raise OtherUserAlreadyInControl(obj.actor.get_user_name())
         return cmd(obj, request)
 
     return wrap
