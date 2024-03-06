606bde624a21552c1beb32dc76d6f4cc61c7308a
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-03-06 23:48:36 +0100

    get rid of internal drunc configurations, harmonise the configuration type, and use the correct function in configuration utils

--- src/drunc/process_manager/interface/cli_argument.py
+++ src/drunc/process_manager/interface/cli_argument.py
@@ -14,15 +14,14 @@ def add_query_options(at_least_one:bool, all_processes_by_default:bool=False):
 
 def accept_configuration_type():
     def configuration_type_callback(ctx, param, conf_type):
-        from drunc.process_manager.utils import ConfTypes
-        CONF_TYPE = conf_type.upper()
-        return ConfTypes[CONF_TYPE]
+        from drunc.utils.configuration import CLI_to_ConfTypes
+        return CLI_to_ConfTypes(conf_type)
 
     def add_decorator(function):
         import click
         f1 = click.argument(
             'conf-type',
-            type=click.Choice(['daqconf', 'drunc', 'OKS'], case_sensitive=False),
+            type=click.Choice(['file', 'oksconfig'], case_sensitive=False),
             #default='daqconf',
             callback=configuration_type_callback
         )(function)
