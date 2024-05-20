96fdaa8dc32dbb6a8122c7fae6bdc25c320ef958
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-05-20 12:26:52 +0200

    generate a NOT_EXECUTED_NOT_IN_CONTROL response when the use isn't in control

--- src/drunc/controller/decorators.py
+++ src/drunc/controller/decorators.py
@@ -6,8 +6,15 @@ def in_control(cmd):
     @wraps(cmd)
     def wrap(obj, request):
         if not obj.actor.token_is_current_actor(request.token):
-            from drunc.controller.exceptions import OtherUserAlreadyInControl
-            raise OtherUserAlreadyInControl()
+            return Response(
+                token = request.token,
+                data = PlainText(
+                    text = f"User {request.token.user_name} is not in control of {obj.__class__.__name__}",
+                ),
+                flag = ResponseFlag.NOT_EXECUTED_NOT_IN_CONTROL,
+                children_responses = {}
+            )
+
         return cmd(obj, request)
 
     return wrap
