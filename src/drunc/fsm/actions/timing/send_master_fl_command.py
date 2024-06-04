b86df85a1d1038198ec5795ac45222820ecf5ea6
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-06-04 12:58:22 +0200

    command validation example

--- src/drunc/fsm/actions/timing/send_master_fl_command.py
+++ src/drunc/fsm/actions/timing/send_master_fl_command.py
@@ -7,18 +7,23 @@ class SendMasterFLCommand(FSMAction):
             name = "send-master-fl-command"
         )
         #self.conf_dict = {p.name: p.value for p in configuration.parameters}
+        self.commands = {
+            "time_sync_echo": 1,
+            #...
+        }
+
 
     def pre_send_master_fl_command(
         self,
         _input_data,
         _context,
-        fl_cmd_id:int,
+        fl_cmd_id:str,
         channel:int,
         number_of_commands_to_send:int,
         **kwargs
     ):
         # parse fl_cmd_id...
-        _input_data['fl_cmd_id'] = fl_cmd_id
+        _input_data['fl_cmd_id'] = self.commands[fl_cmd_id]
         _input_data['channel'] = channel
         _input_data['number_of_commands_to_send'] = number_of_commands_to_send
 
