a9b7436891f4a7f5844cacd4f5ac36deb6e0c7f3
Stoyan Trilov <stoyan.trilov@cern.ch>
2024-09-23 18:00:16 +0100

    timing command rename

--- src/drunc/fsm/actions/timing/master_send_fl_command.py
+++ src/drunc/fsm/actions/timing/master_send_fl_command.py
@@ -1,29 +1,23 @@
 from drunc.fsm.core import FSMAction
 from drunc.utils.utils import now_str
 
-class SendMasterFLCommand(FSMAction):
+class MasterSendFLCommand(FSMAction):
     def __init__(self, configuration):
         super().__init__(
-            name = "send-master-fl-command"
+            name = "master-send-fl-command"
         )
-        #self.conf_dict = {p.name: p.value for p in configuration.parameters}
-        self.commands = {
-            "time_sync_echo": 1,
-            #...
-        }
 
-
-    def pre_send_master_fl_command(
+    def pre_master_send_fl_command(
         self,
         _input_data,
         _context,
-        fl_cmd_id:str,
+        fl_cmd_id:int,
         channel:int,
         number_of_commands_to_send:int,
         **kwargs
     ):
         # parse fl_cmd_id...
-        _input_data['fl_cmd_id'] = self.commands[fl_cmd_id]
+        _input_data['fl_cmd_id'] = fl_cmd_id
         _input_data['channel'] = channel
         _input_data['number_of_commands_to_send'] = number_of_commands_to_send
 
