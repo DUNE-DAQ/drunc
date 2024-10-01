f12a0f52e05bf53fd6635a064c485a27665fb100
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-10-01 18:05:53 +0200

    just copy the env

--- src/drunc/fsm/actions/thread_pinning.py
+++ src/drunc/fsm/actions/thread_pinning.py
@@ -13,16 +13,13 @@ class ThreadPinning(FSMAction):
         self.conf_dict = {p.name: p.value for p in configuration.parameters}
 
     def pin_thread(self, thread_pinning_file, configuration, session):
-        from drunc.process_manager.oks_parser import collect_apps, required_env_keys
+        from drunc.process_manager.oks_parser import collect_apps
         import conffwk
         db = conffwk.Configuration(f"oksconflibs:{configuration}")
         session_dal = db.get_dal(class_name="Session", uid=session)
 
         from os import environ
         env = environ.copy()
-        for key in required_env_keys:
-            if key not in required_env_keys:
-                del env[key]
 
         apps = collect_apps(db, session_dal, session_dal.segment, environ)
 
