d801662ca17bbd28f96c9f61ca2d8b5b26a36687
John Freeman <jcfree@daq.fnal.gov>
2024-10-17 16:02:15 -0500

    JCF: this commit perfectly recreates the state of the code at the head of the plasorak/no-stack-on-failed-lookup branch, commit e5d88ede0b752fcada738c6544eee93004d9172a

--- src/drunc/connectivity_service/client.py
+++ src/drunc/connectivity_service/client.py
@@ -26,14 +26,14 @@ class ConnectivityServiceClient:
             # assume the simplest case here
             self.address = f'http://{address}'
 
-    def retract(self, uid, data_type:str, break_on_unreachable:bool=False):
+    def retract(self, uid):
         from drunc.utils.utils import http_post
         data = {
             'partition': self.session,
             'connections': [
                 {
                     'connection_id': uid,
-                    'data_type': data_type,
+                    'data_type': 'run-control-messages',
                 }
             ]
         }
@@ -59,14 +59,9 @@ class ConnectivityServiceClient:
                 r.raise_for_status()
                 break
             except (HTTPError, ConnectionError) as e:
-
-                if break_on_unreachable and isinstance(e, ConnectionError):
-                    self.logger.warning('Connectivity service seems unreachable, assuming it\'s already been killed')
-                    break
-
                 from time import sleep
                 sleep(0.5)
-
+                continue
 
 
 
