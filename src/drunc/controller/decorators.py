3cf3d0b4eabfde6e42a6c5318a21855bc2133d29
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-10-22 13:00:42 -0500

    add logging

--- src/drunc/controller/decorators.py
+++ src/drunc/controller/decorators.py
@@ -1,14 +1,14 @@
+import logging
 
 def in_control(cmd):
-
+    log = logging.getLogger('in_control')
     from functools import wraps
 
     @wraps(cmd)
     def wrap(obj, request):
-        print(f'actor is {obj.actor._token}, requester is {request.token}')
 
         if not obj.actor.token_is_current_actor(request.token):
-            print(f'FAILED actor is {obj.actor._token}, requester is {request.token}')
+            log.error(f'Actor is {obj.actor._token}, requester is {request.token}, user is not in control')
 
             from druncschema.request_response_pb2 import Response, ResponseFlag
             from druncschema.generic_pb2 import PlainText
@@ -32,6 +32,8 @@ def in_control(cmd):
 
 
 def with_command_lock(cmd):
+    log = logging.getLogger('with_command_lock')
+
     from functools import wraps
 
     @wraps(cmd)
@@ -42,6 +44,8 @@ def with_command_lock(cmd):
             obj._command_lock.release()
             return ret
         else:
+            log.error(f'Someone is already executing a command on this controller, lasting more than 10 seconds!')
+
             return Response(
                 name = obj.name,
                 token = request.token,
