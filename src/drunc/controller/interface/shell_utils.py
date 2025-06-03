d8e6da82a51818dc8c46eee55a0adb36c0126892
John Freeman <jcfree@daq.fnal.gov>
2025-06-03 10:23:11 -0500

    JCF: with Pawel's help, fix AttributeError: 'UnifiedShellContext' object has no attribute 'critical' error

--- src/drunc/controller/interface/shell_utils.py
+++ src/drunc/controller/interface/shell_utils.py
@@ -255,9 +255,9 @@ def controller_setup(ctx, controller_address):
                 time.sleep(1)
 
             except Exception as e:
-                ctx.critical("Could not get the controller's status")
-                ctx.critical(e)
-                ctx.critical("Exiting.")
+                log.critical("Could not get the controller's status")
+                log.critical(e)
+                log.critical("Exiting.")
                 ctx.terminate()
                 raise e
 
