b4d1ef06f1079b955350b43c660054ec0dde2ed0
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-06-26 12:57:26 +0200

    restore utests

--- src/drunc/tests/issues/test_issue309.py
+++ src/drunc/tests/issues/test_issue309.py
@@ -3,13 +3,13 @@
 from drunc.controller.configuration import ControllerConfHandler
 from drunc.utils.utils import setup_root_logger
 
-
 def test_issue309(load_test_config):
     setup_root_logger("INFO")
     from drunc.utils.configuration import OKSKey, parse_conf_url
 
     conf_path, conf_type = parse_conf_url("oksconflibs:deep-segments-config.data.xml")
     controller_id = "controller-3"
+
     controller_configuration = ControllerConfHandler(
         type=conf_type,
         data=conf_path,
@@ -19,6 +19,7 @@ def test_issue309(load_test_config):
             obj_uid=controller_id,
             session="deep-segments-config",  # some of the function for enable/disable require the full dal of the session
         ),
+        session_name="test",
     )
 
     assert controller_configuration.data.controller.id == controller_id
--- src/drunc/tests/issues/test_issue363.py
+++ src/drunc/tests/issues/test_issue363.py
@@ -4,11 +4,11 @@ from drunc.controller.configuration import ControllerConfHandler
 from drunc.utils.configuration import OKSKey, parse_conf_url
 from drunc.utils.utils import setup_root_logger
 
-
 def test_issue363(load_test_config):
     setup_root_logger("INFO")
     conf_path, conf_type = parse_conf_url("oksconflibs:nestedConfig.data.xml")
     controller_id = "nested-segment-controller"
+
     controller_configuration = ControllerConfHandler(
         type=conf_type,
         data=conf_path,
@@ -18,6 +18,7 @@ def test_issue363(load_test_config):
             obj_uid=controller_id,
             session="test-config",  # some of the function for enable/disable require the full dal of the session
         ),
+        session_name="test",
     )
     ids = [segment.id for segment in controller_configuration.data.segments]
     assert ids == ["bottom-segment-1", "bottom-segment-2"]
