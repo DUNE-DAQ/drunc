89323803dbeb2786a8c48cb7f57c8d8506493613
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-07-10 00:04:57 +0200

    send sighup to children

--- src/drunc/apps/__main_process_wrapper__.py
+++ src/drunc/apps/__main_process_wrapper__.py
@@ -1,32 +1,50 @@
-import signal
 import os
-import time
+import signal
 import subprocess
+import time
+
 import click
 
+from drunc.process_manager.popen_process_manager import on_parent_exit
+
+
 def terminate_all(sig, frame):
-    print('')
     pgrp = os.getpgid(os.getpid())
     os.killpg(pgrp, signal.SIGKILL)
 
 
 @click.command()
+@click.option(
+    "-l",
+    "--log",
+    "log_path",
+    type=click.Path(file_okay=True, dir_okay=False),
+    required=True,
+)
 @click.argument("cmd")
-@click.option("-l", "--log", "log_path", type=click.Path(file_okay=True, dir_okay=False), required=True)
 def main(cmd: str, log_path: str):
     signal.signal(signal.SIGHUP, terminate_all)
+    signal.signal(signal.SIGINT, terminate_all)
 
-    with open(log_path, 'w') as logfile:
-        proc = subprocess.Popen(cmd, shell=True, stdout=logfile, stderr=logfile)
-        print(f"Started process with pid {proc.pid}")
+    with open(log_path, "w") as logfile:
+        proc = subprocess.Popen(
+            cmd,
+            shell=True,
+            stdout=logfile,
+            stderr=logfile,
+            preexec_fn=on_parent_exit(
+                signal.SIGHUP
+            ),  # Propagate SIGHUP to child processes, SIGKILL doesn't seem to kill gunicorn...
+        )
+        # print(f"Started process with pid {proc.pid}")
 
         return_code = None
         while True:
             return_code = proc.poll()
             if return_code is not None:
-                break
+                return return_code
             time.sleep(0.1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
