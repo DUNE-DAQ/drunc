64613db0c04ef0d747c9e49abf3d660e05c28b40
Aurash Karimi <a.karimi@imperial.ac.uk>
2025-09-17 17:19:47 +0100

    increase keepalive time for grpc further

--- src/drunc/grpc_settings.py
+++ src/drunc/grpc_settings.py
@@ -1,24 +1,14 @@
 # see https://github.com/grpc/grpc/blob/v1.74.x/include/grpc/impl/channel_arg_names.h
 # for a list of all possible gRPC channel arguments
+# gRPC keepalive reference: https://github.com/grpc/grpc/blob/master/doc/keepalive.md
 
-MANAGER_SERVER_GRPC_CONFIG = [
-    (
-        "grpc.http2.min_ping_interval_without_data_ms",
-        45_000,
-    ),
-]
+MANAGER_SERVER_GRPC_CONFIG = []
 MANAGER_CLIENT_GRPC_CONFIG = [
-    ("grpc.keepalive_time_ms", 90_000),
+    ("grpc.keepalive_time_ms", 600_000),
 ]
-CONTROLLER_SERVER_GRPC_CONFIG = [
-    (
-        "grpc.http2.min_ping_interval_without_data_ms",
-        45_000,
-    ),
-]
-
+CONTROLLER_SERVER_GRPC_CONFIG = []
 CONTROLLER_CLIENT_GRPC_CONFIG = [
-    ("grpc.keepalive_time_ms", 90_000),
+    ("grpc.keepalive_time_ms", 600_000),
 ]
 
 MANAGER_SERVER_GRPC_MAX_WORKERS = 10
