c34489bee9dbd49fd396b89386455be2ceca61d1
Miruna Serian <mirunaserian@gmail.com>
2025-10-01 12:32:23 +0100

    remove change in commands.py

--- src/drunc/process_manager/interface/commands.py
+++ src/drunc/process_manager/interface/commands.py
@@ -176,7 +176,7 @@ def kill(obj: ProcessManagerContext, query: ProcessQuery) -> None:
     obj.print(
         tabulate_process_instance_list(result, "Killed process", False)
     )  # rich tables require console printing
-    obj.delete_driver("controller")
+
 
 @click.command("flush")
 @add_query_options(at_least_one=False, all_processes_by_default=True)
