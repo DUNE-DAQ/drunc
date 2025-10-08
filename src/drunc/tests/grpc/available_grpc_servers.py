1c63e87d1c14cfc9696344f49b90280ca7881ac1
Aurash Karimi <a.karimi@imperial.ac.uk>
2025-10-08 18:13:55 +0100

    remove redundant files

--- src/drunc/tests/grpc/available_grpc_servers.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ServerType(Enum):
-    """Enumeration of supported server types with their corresponding CLI scripts."""
-
-    MANAGER = 0
-    ROOT_CONTROLLER = 1
-    CHILD_CONTROLLER = 2
