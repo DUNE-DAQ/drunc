009c5ef1e3bd9437ad2657d11e2384bcec10299a
PawelPlesniak <plesniakpaul@gmail.com>
2025-11-18 17:20:32 +0100

    RESTART WOOP WOOP

--- src/drunc/fsm/actions/db_run_registry.py
+++ src/drunc/fsm/actions/db_run_registry.py
@@ -35,80 +35,108 @@ class DBRunRegistry(FSMAction):
         self.timeout = 2
 
     def pre_start(self, _input_data: dict, _context, **kwargs):
+        """
+        Upload a copy of the XML and JSON configurations used to take the current run to
+        the Run Registry, and insert a new run number entry.
+
+        Args:
+            _input_data (dict): Input data dictionary containing run information.
+            _context: Context object containing configuration and state information.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: The input data dictionary, unchanged.
+
+        Raises:
+            CannotGetSoftwareVersion: If the software version cannot be determined.
+            CannotInsertRunNumber: If there is an error inserting the run number into
+                the Run Registry
+        """
+
+        # Seems like run_number isn't in _input_data in post_drain_dataflow so need to
+        # initialise it here
         self.run_number = _input_data[
             "run"
-        ]  # Seems like run_number isn't in _input_data in post_drain_dataflow so need to initialise it here
+        ]
 
-        run_type = _input_data.get("production_vs_test", "TEST")
-        det_id = _context.configuration.db.get_dal(
-            class_name="Session", uid=_context.configuration.oks_key.session
-        ).detector_configuration.id
+        # Get the environment variables for upload
         software_version = os.getenv("DUNE_DAQ_BASE_RELEASE")
         if software_version == None:
             raise CannotGetSoftwareVersion()
 
-        f_xml = tempfile.NamedTemporaryFile(suffix=".data.xml", delete=True)
-        f_json = tempfile.NamedTemporaryFile(suffix=".data.json", delete=True)
-        f_entry_point = tempfile.NamedTemporaryFile(
-            suffix="_entry_point.txt", delete=True
+        # Get the input data variables for upload
+        run_type = _input_data.get("production_vs_test", "TEST")
+
+        # Get the configuration variables for upload
+        conf = _context.configuration
+        det_id = conf.db.get_dal(
+            class_name="Session", uid=_context.configuration.oks_key.session
+        ).detector_configuration.id
+
+        # Create a consolidated XML configuration file
+        xml_file = tempfile.NamedTemporaryFile(suffix=".data.xml", delete=False)
+        xml_filename = xml_file.name
+        consolidate_db(_context.configuration.initial_data.split(":")[1], xml_filename)
+
+        # Create a JSON configuration file
+        json_file = tempfile.NamedTemporaryFile(suffix=".data.json", delete=False)
+        json_filename = json_file.name
+        jsonify_xml_data(xml_name, json_filename)
+
+        # Create a entry point file (contains the session key)
+        entry_point_file = tempfile.NamedTemporaryFile(
+            suffix="_entry_point.txt", delete=False
         )
-        xml_name = f_xml.name
-        json_name = f_json.name
-        entry_point_name = f_entry_point.name
+        entry_point_filename = entry_point_file.name
+        with open(entry_point_filename, "w") as f:
+            f.write(conf.oks_key.session)
 
-        consolidate_db(_context.configuration.initial_data.split(":")[1], xml_name)
-        jsonify_xml_data(xml_name, json_name)
-        with open(entry_point_name, "w") as f:
-            f.write(_context.configuration.oks_key.session)
+        tarball = tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False)
+        tarball_name = tarball.name
 
-        f_tar = tempfile.NamedTemporaryFile(
-            suffix=".tar.gz",
-            delete=False,  # delete when f_tar gets out of scope
-            # for after python 3.12...
-            # delete = True, # delete when f_tar gets out of scope
-            # delete_on_close = False
-        )
-        tar_name = f_tar.name
+        with tarfile.open(fileobj=tarball, mode="w:gz") as tar:
+            tar.add(xml_filename, arcname=os.path.basename(xml_filename))
+            tar.add(json_filename, arcname=os.path.basename(json_filename))
+            tar.add(entry_point_filename, arcname=os.path.basename(entry_point_filename))
+        # f_tar.close()
 
-        with tarfile.open(fileobj=f_tar, mode="w:gz") as tar:
-            tar.add(xml_name, arcname=os.path.basename(xml_name))
-            tar.add(json_name, arcname=os.path.basename(json_name))
-            tar.add(entry_point_name, arcname=os.path.basename(entry_point_name))
-        f_tar.close()
+        
 
-        with open(tar_name, "rb") as f:
-            files = {"file": f}
-            post_data = {
-                "run_num": self.run_number,
-                "det_id": det_id,
-                "run_type": run_type,
-                "software_version": software_version,
-            }
+        # with open(tar_name, "rb") as f:
+        #     files = {"file": f}
+        #     post_data = {
+        #         "run_num": self.run_number,
+        #         "det_id": det_id,
+        #         "run_type": run_type,
+        #         "software_version": software_version,
+        #     }
 
-            try:
-                r = requests.post(
-                    self.API_SOCKET + "/runregistry/insertRun/",
-                    files=files,
-                    data=post_data,
-                    auth=(self.API_USER, self.API_PSWD),
-                    timeout=self.timeout,
-                )
-                r.raise_for_status()
-            except requests.HTTPError as exc:
-                error = f"of HTTP Error (maybe failed auth, maybe ill-formed post message, ...) using {__name__}"
-                self.log.error(error)
-                raise CannotInsertRunNumber(error) from exc
-            except requests.ConnectionError as exc:
-                error = f"connection to {self.API_SOCKET} wasn't successful using {__name__}"
-                self.log.error(error)
-                raise CannotInsertRunNumber(error) from exc
-            except requests.Timeout as exc:
-                error = f"connection to {self.API_SOCKET} timed out using {__name__}"
-                self.log.error(error)
-                raise CannotInsertRunNumber(error) from exc
+        #     try:
+        #         r = requests.post(
+        #             self.API_SOCKET + "/runregistry/insertRun/",
+        #             files=files,
+        #             data=post_data,
+        #             auth=(self.API_USER, self.API_PSWD),
+        #             timeout=self.timeout,
+        #         )
+        #         r.raise_for_status()
+        #     except requests.HTTPError as exc:
+        #         error = f"of HTTP Error (maybe failed auth, maybe ill-formed post message, ...) using {__name__}"
+        #         self.log.error(error)
+        #         raise CannotInsertRunNumber(error) from exc
+        #     except requests.ConnectionError as exc:
+        #         error = f"connection to {self.API_SOCKET} wasn't successful using {__name__}"
+        #         self.log.error(error)
+        #         raise CannotInsertRunNumber(error) from exc
+        #     except requests.Timeout as exc:
+        #         error = f"connection to {self.API_SOCKET} timed out using {__name__}"
+        #         self.log.error(error)
+        #         raise CannotInsertRunNumber(error) from exc
 
-        # can be removed if we use delete_on_close=False in f_tar
-        os.remove(tar_name)
+        # Clean up
+        for file in [xml_name, json_name, entry_point_name, tar_name]:
+            os.remove(file)
+
         return _input_data
 
     def post_drain_dataflow(self, _input_data, _context, **kwargs):
