f221aef0d70e234a1b943461c965aa6a8ef08736
Miruna Serian <mirunaserian@gmail.com>
2025-11-19 14:19:06 +0000

    add mkdocs hook to fix nav titles

---
+++ docs/utils/mkdocs_hooks.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+def on_nav(env, config, files):
+    for file in files:
+        if file.page:
+            # Restore origin file name (without extension) as title.
+            file.page.title = Path(file.src_uri).stem
