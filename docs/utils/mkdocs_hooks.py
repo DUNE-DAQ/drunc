735c5113b204a24c6a14987cd019f691799c7804
Miruna Serian <mirunaserian@gmail.com>
2025-11-19 14:24:15 +0000

    mkdocs hook - fix index files

--- docs/utils/mkdocs_hooks.py
+++ docs/utils/mkdocs_hooks.py
@@ -4,4 +4,6 @@ def on_nav(env, config, files):
     for file in files:
         if file.page:
             # Restore origin file name (without extension) as title.
+            if "index" in str(Path(file.src_uri).stem):
+                continue
             file.page.title = Path(file.src_uri).stem
