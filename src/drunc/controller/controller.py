50256a88f0f28ccf59d8e1a019415fb2f7f98954
PawelPlesniak <plesniakpaul@gmail.com>
2025-11-19 15:12:30 +0100

    Investigating for error state propagation

--- src/drunc/controller/controller.py
+++ src/drunc/controller/controller.py
@@ -1013,7 +1013,6 @@ class Controller(ControllerServicer):
                 transition_data=command.data,
                 ctx=self,
             )
-            self.log.critical(f"PP: {fsm_data=}")
 
             # If the command publishes to ELisa Logbook, make sure that .dotdrunc.json
             # is present and well formatted
