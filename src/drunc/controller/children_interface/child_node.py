a31379786261aee45b5c51c0d390de15e1bac58c
James Paul Turner <james.turner@imperial.ac.uk>
2025-12-12 11:50:48 +0000

    Remove unused ChildInterfaceTechnologyUnknown.

--- src/drunc/controller/children_interface/child_node.py
+++ src/drunc/controller/children_interface/child_node.py
@@ -16,16 +16,10 @@ from druncschema.controller_pb2 import (
     WhoIsInChargeResponse,
 )
 
-from drunc.exceptions import DruncSetupException
 from drunc.utils.utils import (
     ControlType,
     get_logger,
 )
-
-
-class ChildInterfaceTechnologyUnknown(DruncSetupException):
-    def __init__(self, t, name):
-        super().__init__(f"The type {t} is not supported for the ChildNode {name}")
 
 
 class ChildNode(ABC):
