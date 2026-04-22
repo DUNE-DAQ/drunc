f6d9e41183b7d12a3c6ed6da93f7de3c8884ca1a
Aurash Karimi <a.karimi@imperial.ac.uk>
2026-04-22 10:02:25 +0100

    fix mypy

--- src/drunc/controller/configuration.py
+++ src/drunc/controller/configuration.py
@@ -252,18 +252,20 @@ class ControllerConfHandler(ConfHandler):
 
         match ctype:
             case ControlType.gRPC:
-                conf_handler = gRCPChildConfHandler(configuration, ConfTypes.PyObject)
+                grpc_conf_handler = gRCPChildConfHandler(
+                    configuration, ConfTypes.PyObject
+                )
                 return gRPCChildNode(
-                    name, conf_handler, uri, connectivity_service, init_token
+                    name, grpc_conf_handler, uri, connectivity_service, init_token
                 )
 
             case ControlType.REST_API:
-                conf_handler = RESTAPIChildNodeConfHandler(
+                restapi_conf_handler = RESTAPIChildNodeConfHandler(
                     configuration, ConfTypes.PyObject
                 )
                 return RESTAPIChildNode(
                     name,
-                    conf_handler,
+                    restapi_conf_handler,
                     uri,
                     self.data.controller.fsm,
                     connectivity_service=connectivity_service,
