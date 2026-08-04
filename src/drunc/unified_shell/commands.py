5fc0a4d1c26992c31dee28391067c7f8c2bb5cc5
PawelPlesniak <plesniakpaul@gmail.com>
2026-08-04 15:06:40 +0200

    Remvoing resisudal time.sleep

--- src/drunc/unified_shell/commands.py
+++ src/drunc/unified_shell/commands.py
@@ -1,6 +1,5 @@
 import getpass
 import sys
-import time
 from functools import update_wrapper
 
 import click
@@ -150,7 +149,6 @@ def boot(
         status_process_count += 1
 
     if ps_process_count != status_process_count:
-        # time.sleep(1)
         log.debug(  # TODO - once issue 793 is resolved, this should be a log.error
             f"Booted, but the number of processes registered with the process manager "
             f"({ps_process_count}) does not match the number of processes registered "
@@ -179,7 +177,6 @@ def boot(
     )
     dead_process_count = expected_booted_processes - alive_process_count
     if dead_process_count > 0:
-        time.sleep(1)
         log.error(
             f"Booted, but {dead_process_count} processes died. Use the [yellow]ps[/] "
             "command to find out which applications are dead, and [yellow]logs[/] "
